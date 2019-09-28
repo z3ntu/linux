38583c9945fd7be20a474a32f8603f7c862a9b2f
Luca Weiss <luca@z3ntu.xyz>
2019-09-28 15:30:16 +0200

    v5

--- drivers/power/supply/qcom_bms.c
+++ drivers/power/supply/qcom_bms.c
@@ -7,10 +7,11 @@
  */
 
 #include <linux/module.h>
-#include <linux/param.h>
+#include <linux/fixp-arith.h>
 #include <linux/platform_device.h>
 #include <linux/power_supply.h>
 #include <linux/slab.h>
+#include <linux/bitops.h>
 #include <linux/of.h>
 #include <linux/of_platform.h>
 #include <linux/regmap.h>
@@ -26,9 +27,6 @@
 #define BMS_HOLD_OREG_DATA		BIT(0)
 #define BMS_CLEAR_SHDW_CC		BIT(6)
 
-#define CC_36_BIT_MASK			0xFFFFFFFFFLL
-#define SIGN_EXTEND_36_TO_64_MASK	(-1LL ^ CC_36_BIT_MASK)
-
 #define BMS_CC_READING_RESOLUTION_N	542535
 #define BMS_CC_READING_RESOLUTION_D	10000
 #define BMS_CC_READING_TICKS		56
@@ -43,121 +41,99 @@ struct bms_ocv_lut {
 	int rows;
 	s8 temp_legend[TEMPERATURE_COLS];
 	u8 capacity_legend[MAX_CAPACITY_ROWS];
-	u16 lut[MAX_CAPACITY_ROWS][TEMPERATURE_COLS];
+	u32 lut[MAX_CAPACITY_ROWS][TEMPERATURE_COLS];
 };
 
 /* lookup table for battery temperature -> fcc conversion */
 struct bms_fcc_lut {
 	s8 temp_legend[TEMPERATURE_COLS];
-	u16 lut[TEMPERATURE_COLS];
+	u32 lut[TEMPERATURE_COLS];
 };
 
 struct bms_device_info {
 	struct device *dev;
 	struct regmap *regmap;
-	struct power_supply *bat;
-	struct power_supply_desc bat_desc;
 	struct bms_ocv_lut ocv_lut;
+	struct power_supply_desc bat_desc;
 	struct bms_fcc_lut fcc_lut;
 	struct iio_channel *adc;
-	spinlock_t bms_output_lock;
-	int base_addr;
+	struct mutex bms_output_lock;
+	u32 base_addr;
 
 	int ocv_thr_irq;
-	int ocv;
+	u32 ocv;
 };
 
-static s64 sign_extend_s36(uint64_t raw)
-{
-	raw = raw & CC_36_BIT_MASK;
-
-	return (raw >> 35) == 0LL ?
-		raw : (SIGN_EXTEND_36_TO_64_MASK | raw);
-}
-
-static unsigned int interpolate(int y0, int x0, int y1, int x1, int x)
-{
-	if (y0 == y1 || x == x0)
-		return y0;
-	if (x1 == x0 || x == x1)
-		return y1;
-
-	return y0 + ((y1 - y0) * (x - x0) / (x1 - x0));
-}
-
-static unsigned int between(int left, int right, int val)
+static bool between(int left, int right, int val)
 {
 	if (left <= val && val <= right)
-		return 1;
+		return true;
 
-	return 0;
+	if (left >= val && val >= right)
+		return true;
+
+	return false;
 }
 
-static unsigned int interpolate_capacity(int temp, u16 ocv,
-				struct bms_ocv_lut ocv_lut)
+static int interpolate_capacity(int temp, u32 ocv,
+				struct bms_ocv_lut *ocv_lut)
 {
-	unsigned int pcj_minus_one = 0, pcj = 0;
-	int i, j;
+	int pcj_minus_one = 0, pcj = 0, i2 = 0, i3 = 0, i, j;
 
 	for (j = 0; j < TEMPERATURE_COLS; j++)
-		if (temp <= ocv_lut.temp_legend[j])
+		if (temp <= ocv_lut->temp_legend[j])
 			break;
 
-	if (ocv >= ocv_lut.lut[0][j])
-		return ocv_lut.capacity_legend[0];
+	if (ocv >= ocv_lut->lut[0][j])
+		return ocv_lut->capacity_legend[0];
 
-	if (ocv <= ocv_lut.lut[ocv_lut.rows - 1][j - 1])
-		return ocv_lut.capacity_legend[ocv_lut.rows - 1];
+	if (ocv <= ocv_lut->lut[ocv_lut->rows-1][j-1])
+		return ocv_lut->capacity_legend[ocv_lut->rows-1];
 
-	for (i = 0; i < ocv_lut.rows - 1; i++) {
-		if (pcj == 0 && between(ocv_lut.lut[i][j],
-					ocv_lut.lut[i+1][j], ocv))
-			pcj = interpolate(ocv_lut.capacity_legend[i],
-					  ocv_lut.lut[i][j],
-					  ocv_lut.capacity_legend[i + 1],
-					  ocv_lut.lut[i+1][j],
-					  ocv);
+	for (i = 0; i < ocv_lut->rows-1; i++) {
+		if (between(ocv_lut->lut[i][j],
+			    ocv_lut->lut[i+1][j], ocv))
+			i2 = i;
 
-		if (pcj_minus_one == 0 && between(ocv_lut.lut[i][j-1],
-						  ocv_lut.lut[i+1][j-1], ocv))
-			pcj_minus_one = interpolate(ocv_lut.capacity_legend[i],
-						    ocv_lut.lut[i][j-1],
-						    ocv_lut.capacity_legend[i + 1],
-						    ocv_lut.lut[i+1][j-1],
-						    ocv);
-
-		if (pcj && pcj_minus_one)
-			return interpolate(pcj_minus_one,
-					   ocv_lut.temp_legend[j-1],
-					   pcj,
-					   ocv_lut.temp_legend[j],
-					   temp);
+		if (between(ocv_lut->lut[i][j-1],
+			    ocv_lut->lut[i+1][j-1], ocv))
+			i3 = i;
 	}
 
-	if (pcj)
-		return pcj;
+	/* interpolate two capacities */
+	pcj = fixp_linear_interpolate(ocv_lut->lut[i2][j],
+				      ocv_lut->capacity_legend[i2],
+				      ocv_lut->lut[i2+1][j],
+				      ocv_lut->capacity_legend[i2+1],
+				      ocv);
 
-	if (pcj_minus_one)
-		return pcj_minus_one;
+	pcj_minus_one = fixp_linear_interpolate(ocv_lut->lut[i3][j-1],
+						ocv_lut->capacity_legend[i3],
+						ocv_lut->lut[i3+1][j-1],
+						ocv_lut->capacity_legend[i3+1],
+						ocv);
 
-	return 100;
+	/* interpolate them with the battery temperature */
+	return fixp_linear_interpolate(ocv_lut->temp_legend[j-1],
+				       pcj_minus_one,
+				       ocv_lut->temp_legend[j],
+				       pcj,
+				       temp);
 }
 
-static unsigned long interpolate_fcc(int temp, struct bms_fcc_lut fcc_lut)
+static int interpolate_fcc(int temp, struct bms_fcc_lut *fcc_lut)
 {
-	int i, fcc_mv;
+	int i;
 
 	for (i = 0; i < TEMPERATURE_COLS; i++)
-		if (temp <= fcc_lut.temp_legend[i])
+		if (temp <= fcc_lut->temp_legend[i])
 			break;
 
-	fcc_mv = interpolate(fcc_lut.lut[i - 1],
-			     fcc_lut.temp_legend[i - 1],
-			     fcc_lut.lut[i],
-			     fcc_lut.temp_legend[i],
+	return fixp_linear_interpolate(fcc_lut->temp_legend[i-1],
+			     fcc_lut->lut[i-1],
+			     fcc_lut->temp_legend[i],
+			     fcc_lut->lut[i],
 			     temp);
-
-	return fcc_mv * 10000;
 }
 
 static int bms_lock_output_data(struct bms_device_info *di)
@@ -167,17 +143,17 @@ static int bms_lock_output_data(struct bms_device_info *di)
 	ret = regmap_update_bits(di->regmap, di->base_addr +
 				 REG_BMS_CC_DATA_CTL,
 				 BMS_HOLD_OREG_DATA, BMS_HOLD_OREG_DATA);
-	if (ret < 0) {
+	if (ret) {
 		dev_err(di->dev, "failed to lock bms output: %d", ret);
 		return ret;
 	}
 
 	/*
-	 * Sleep for 100 microseconds here to make sure there has
-	 * been at least three cycles of the sleep clock so that
-	 * the registers are correctly locked.
+	 * Sleep for at least 100 microseconds here to make sure
+	 * there has been at least three cycles of the sleep clock
+	 * so that the registers are correctly locked.
 	 */
-	udelay(100);
+	usleep_range(100, 1000);
 
 	return 0;
 }
@@ -189,7 +165,7 @@ static int bms_unlock_output_data(struct bms_device_info *di)
 	ret = regmap_update_bits(di->regmap, di->base_addr +
 				 REG_BMS_CC_DATA_CTL,
 				 BMS_HOLD_OREG_DATA, 0);
-	if (ret < 0) {
+	if (ret) {
 		dev_err(di->dev, "failed to unlock bms output: %d", ret);
 		return ret;
 	}
@@ -197,63 +173,64 @@ static int bms_unlock_output_data(struct bms_device_info *di)
 	return 0;
 }
 
-static int bms_read_ocv(struct bms_device_info *di, int *ocv)
+static int bms_read_ocv(struct bms_device_info *di, u32 *ocv)
 {
-	unsigned long flags;
 	int ret;
 	u16 read_ocv;
 
-	spin_lock_irqsave(&di->bms_output_lock, flags);
+	mutex_lock(&di->bms_output_lock);
 
 	ret = bms_lock_output_data(di);
-	if (ret < 0)
+	if (ret)
 		goto err_lock;
 
-	ret = regmap_bulk_read(di->regmap, di->base_addr +
+	ret = regmap_bulk_read(di->regmap, di->base_addr+
 			       REG_BMS_OCV_FOR_SOC_DATA0, &read_ocv, 2);
-	if (ret < 0) {
-		dev_err(di->dev, "OCV read failed: %d", ret);
-		return ret;
+	if (ret) {
+		dev_err(di->dev, "open circuit voltage read failed: %d", ret);
+		goto err_read;
 	}
 
-	dev_dbg(di->dev, "read OCV value of: %d", read_ocv);
-	*ocv = read_ocv;
+	dev_dbg(di->dev, "read open circuit voltage of: %d mv", read_ocv);
 
-	ret = bms_unlock_output_data(di);
+
+	*ocv = read_ocv * 1000;
+
+err_read:
+	bms_unlock_output_data(di);
 
 err_lock:
-	spin_unlock_irqrestore(&di->bms_output_lock, flags);
+	mutex_unlock(&di->bms_output_lock);
 
 	return ret;
 }
 
 static int bms_read_cc(struct bms_device_info *di, s64 *cc_uah)
 {
-	unsigned long flags;
 	int ret;
 	s64 cc_raw_s36, cc_raw, cc_uv, cc_pvh;
 
-	spin_lock_irqsave(&di->bms_output_lock, flags);
+	mutex_lock(&di->bms_output_lock);
 
 	ret = bms_lock_output_data(di);
-	if (ret < 0)
-		return ret;
+	if (ret)
+		goto err_lock;
 
 	ret = regmap_bulk_read(di->regmap, di->base_addr +
 			       REG_BMS_SHDW_CC_DATA0,
 			       &cc_raw_s36, 5);
-	if (ret < 0) {
+	if (ret) {
 		dev_err(di->dev, "coulomb counter read failed: %d", ret);
-		return ret;
+		goto err_read;
 	}
 
 	ret = bms_unlock_output_data(di);
-	if (ret < 0)
-		return ret;
+	if (ret)
+		goto err_lock;
 
-	spin_unlock_irqrestore(&di->bms_output_lock, flags);
+	mutex_unlock(&di->bms_output_lock);
 
-	cc_raw = sign_extend_s36(cc_raw_s36);
+	cc_raw = sign_extend32(cc_raw_s36, 28);
 
 	/* convert raw to uv */
 	cc_uv = div_s64(cc_raw * BMS_CC_READING_RESOLUTION_N,
@@ -261,50 +238,57 @@ static int bms_read_cc(struct bms_device_info *di, s64 *cc_uah)
 
 	/* convert uv to pvh */
 	cc_pvh = div_s64(cc_uv * BMS_CC_READING_TICKS * 100000,
-			 BMS_SLEEP_CLK_HZ * SECONDS_PER_HOUR) * 10;
+			 BMS_SLEEP_CLK_HZ * SECONDS_PER_HOUR);
 
 	/* divide by impedance */
 	*cc_uah = div_s64(cc_pvh, 10000);
 
-	dev_dbg(di->dev, "read coulomb counter value of: %lld", *cc_uah);
+	dev_dbg(di->dev, "read coulomb counter value of: %lld uah", *cc_uah);
 
 	return 0;
+
+err_read:
+	bms_unlock_output_data(di);
+
+err_lock:
+	mutex_unlock(&di->bms_output_lock);
+
+	return ret;
 }
 
 static void bms_reset_cc(struct bms_device_info *di)
 {
 	int ret;
-	unsigned long flags;
 
-	spin_lock_irqsave(&di->bms_output_lock, flags);
+	mutex_lock(&di->bms_output_lock);
 
 	ret = regmap_update_bits(di->regmap, di->base_addr +
 				 REG_BMS_CC_CLEAR_CTL,
 				 BMS_CLEAR_SHDW_CC,
 				 BMS_CLEAR_SHDW_CC);
-	if (ret < 0) {
+	if (ret) {
 		dev_err(di->dev, "coulomb counter reset failed: %d", ret);
 		goto err_lock;
 	}
 
-	/* wait two sleep cycles for cc to reset */
-	udelay(100);
+	/* wait at least three sleep cycles for cc to reset */
+	usleep_range(100, 1000);
 
 	ret = regmap_update_bits(di->regmap, di->base_addr +
 				 REG_BMS_CC_CLEAR_CTL,
 				 BMS_CLEAR_SHDW_CC, 0);
-	if (ret < 0)
+	if (ret)
 		dev_err(di->dev, "coulomb counter re-enable failed: %d", ret);
 
 err_lock:
-	spin_unlock_irqrestore(&di->bms_output_lock, flags);
+	mutex_unlock(&di->bms_output_lock);
 }
 
 static int bms_calculate_capacity(struct bms_device_info *di, int *capacity)
 {
-	unsigned long ocv_capacity, fcc;
-	int ret, temp, temp_degc;
-	s64 cc, capacity_nodiv;
+	unsigned long fcc;
+	int ret, temp, ocv_capacity, temp_degc;
+	s64 cc = 0;
 
 	ret = iio_read_channel_raw(di->adc, &temp);
 	if (ret < 0) {
@@ -312,7 +296,7 @@ static int bms_calculate_capacity(struct bms_device_info *di, int *capacity)
 		return ret;
 	}
 
-	temp_degc = (temp + 500) / 1000;
+	temp_degc = DIV_ROUND_CLOSEST(temp, 1000);
 
 	ret = bms_read_cc(di, &cc);
 	if (ret < 0) {
@@ -320,12 +304,16 @@ static int bms_calculate_capacity(struct bms_device_info *di, int *capacity)
 		return ret;
 	}
 
-	ocv_capacity = interpolate_capacity(temp_degc, (di->ocv + 5) / 10,
-					    di->ocv_lut);
-	fcc = interpolate_fcc(temp_degc, di->fcc_lut);
+	/* interpolate capacity from open circuit voltage */
+	ocv_capacity = interpolate_capacity(temp_degc, di->ocv,
+					    &di->ocv_lut);
 
-	capacity_nodiv = ((fcc * ocv_capacity) / 100 - cc) * 100;
-	*capacity = div64_ul(capacity_nodiv, fcc);
+	/* interpolate the full charge capacity from temperature */
+	fcc = interpolate_fcc(temp_degc, &di->fcc_lut);
+
+	/* append coloumb counter to capacity */
+	*capacity = DIV_ROUND_CLOSEST(fcc * ocv_capacity, 100);
+	*capacity = div_s64((*capacity - cc) * 100, fcc);
 
 	return 0;
 }
@@ -376,13 +364,12 @@ static int bms_probe(struct platform_device *pdev)
 {
 	struct power_supply_config psy_cfg = {};
 	struct bms_device_info *di;
+	struct power_supply *bat;
 	int ret;
 
 	di = devm_kzalloc(&pdev->dev, sizeof(*di), GFP_KERNEL);
 	if (!di)
 		return -ENOMEM;
-
-	platform_set_drvdata(pdev, di);
 
 	di->dev = &pdev->dev;
 
@@ -401,11 +388,11 @@ static int bms_probe(struct platform_device *pdev)
 		return ret;
 
 	ret = of_property_read_u8_array(di->dev->of_node,
-						 "qcom,ocv-temp-legend",
+						 "qcom,ocv-temp-legend-celsius",
 						 (u8 *)di->ocv_lut.temp_legend,
 						 TEMPERATURE_COLS);
 	if (ret < 0) {
-		dev_err(di->dev, "no ocv temperature legend found");
+		dev_err(di->dev, "no open circuit voltage temperature legend found");
 		return ret;
 	}
 
@@ -414,57 +401,59 @@ static int bms_probe(struct platform_device *pdev)
 						 di->ocv_lut.capacity_legend, 0,
 						 MAX_CAPACITY_ROWS);
 	if (di->ocv_lut.rows < 0) {
-		dev_err(di->dev, "no ocv capacity legend found");
+		dev_err(di->dev, "no open circuit voltage capacity legend found");
 		return ret;
 	}
 
-	ret = of_property_read_variable_u16_array(di->dev->of_node,
-						  "qcom,ocv-lut",
-						  (u16 *)di->ocv_lut.lut,
+	ret = of_property_read_variable_u32_array(di->dev->of_node,
+						  "qcom,ocv-lut-microvolt",
+						  (u32 *)di->ocv_lut.lut,
 						  TEMPERATURE_COLS,
 						  TEMPERATURE_COLS *
 						  MAX_CAPACITY_ROWS);
 	if (ret < 0) {
-		dev_err(di->dev, "no ocv lut array found");
+		dev_err(di->dev, "no open circuit voltage lut array found");
 		return ret;
 	}
 
 	ret = of_property_read_u8_array(di->dev->of_node,
-						 "qcom,fcc-temp-legend",
+						 "qcom,fcc-temp-legend-celsius",
 						 (u8 *)di->fcc_lut.temp_legend,
 						 TEMPERATURE_COLS);
 	if (ret < 0) {
-		dev_err(di->dev, "no fcc temperature legend found");
+		dev_err(di->dev, "no full charge capacity temperature legend found");
 		return ret;
 	}
 
-	ret = of_property_read_u16_array(di->dev->of_node,
-						  "qcom,fcc-lut",
+	ret = of_property_read_u32_array(di->dev->of_node,
+						  "qcom,fcc-lut-microamp-hours",
 						  di->fcc_lut.lut,
 						  TEMPERATURE_COLS);
 	if (ret < 0) {
-		dev_err(di->dev, "no fcc lut array found");
+		dev_err(di->dev, "no full charge capacity lut array found");
 		return ret;
 	}
 
 	ret = bms_read_ocv(di, &di->ocv);
 	if (ret < 0) {
-		dev_err(di->dev, "failed to read initial ocv: %d", ret);
+		dev_err(di->dev, "failed to read initial open circuit voltage: %d",
+			ret);
 		return ret;
 	}
+
+	mutex_init(&di->bms_output_lock);
 
 	di->ocv_thr_irq = platform_get_irq_byname(pdev, "ocv_thr");
 
-	ret = devm_request_irq(di->dev, di->ocv_thr_irq,
+	ret = devm_request_threaded_irq(di->dev, di->ocv_thr_irq, NULL,
 					bms_ocv_thr_irq_handler,
-					IRQF_TRIGGER_RISING,
+					IRQF_TRIGGER_RISING | IRQF_ONESHOT,
 					pdev->name, di);
 	if (ret < 0) {
-		dev_err(di->dev, "failed to request handler for ocv threshold IRQ");
+		dev_err(di->dev, "failed to request handler for open circuit voltage threshold IRQ");
 		return ret;
 	}
 
-	spin_lock_init(&di->bms_output_lock);
 
 	di->bat_desc.name = "bms";
 	di->bat_desc.type = POWER_SUPPLY_TYPE_BATTERY;
@@ -473,11 +462,9 @@ static int bms_probe(struct platform_device *pdev)
 	di->bat_desc.get_property = bms_get_property;
 
 	psy_cfg.drv_data = di;
-	di->bat = devm_power_supply_register(di->dev, &di->bat_desc, &psy_cfg);
-	if (IS_ERR(di->bat))
-		return PTR_ERR(di->bat);
+	bat = devm_power_supply_register(di->dev, &di->bat_desc, &psy_cfg);
 
-	return 0;
+	return PTR_ERR_OR_ZERO(bat);
 }
 
 static const struct of_device_id bms_of_match[] = {
