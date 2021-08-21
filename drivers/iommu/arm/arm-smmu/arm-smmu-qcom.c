4edde7a8fa522056394c678b9c7adfd181350386
Konrad Dybcio <konrad.dybcio@somainline.org>
2021-08-21 19:27:03 +0200

    iommu/arm-smmu-qcom: Add SM8250 DPU compatible
    Add the SM8250 DPU compatible to clients compatible list, as it also needs
    the workarounds.
    
    Signed-off-by: Konrad Dybcio <konrad.dybcio@somainline.org>

--- drivers/iommu/arm/arm-smmu/arm-smmu-qcom.c
+++ drivers/iommu/arm/arm-smmu/arm-smmu-qcom.c
@@ -235,6 +235,7 @@ static const struct of_device_id qcom_smmu_client_of_match[] __maybe_unused = {
 	{ .compatible = "qcom,sc8180x-mdss" },
 	{ .compatible = "qcom,sdm845-mdss" },
 	{ .compatible = "qcom,sdm845-mss-pil" },
+	{ .compatible = "qcom,sm8250-mdss" },
 	{ }
 };
 
