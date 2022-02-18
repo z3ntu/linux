72755d8d84f1d09aa9e7d4939485f7982de1e289
Konrad Dybcio <konrad.dybcio@somainline.org>
2022-02-18 12:44:19 +0100

    [WIP] drm/msm/dpu1: Add SM6350 support
    Add basic SM6350 support to the DPU1 driver to enable display output.
    
    Signed-off-by: Konrad Dybcio <konrad.dybcio@somainline.org>

--- drivers/gpu/drm/msm/disp/dpu1/dpu_kms.c
+++ drivers/gpu/drm/msm/disp/dpu1/dpu_kms.c
@@ -1306,6 +1306,7 @@ static const struct of_device_id dpu_dt_match[] = {
 	{ .compatible = "qcom,sc8180x-dpu", },
 	{ .compatible = "qcom,sc8280xp-dpu", },
 	{ .compatible = "qcom,sm6115-dpu", },
+	{ .compatible = "qcom,sm6350-dpu", },
 	{ .compatible = "qcom,sm8150-dpu", },
 	{ .compatible = "qcom,sm8250-dpu", },
 	{ .compatible = "qcom,sm8350-dpu", },
