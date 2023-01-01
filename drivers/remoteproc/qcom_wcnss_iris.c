9b0b2c456a3fc4e53a75eca08ea0565badf7a6f0
Matti Lehtimäki <matti.lehtimaki@gmail.com>
2023-01-01 18:34:46 +0200

    remoteproc: qcom_wcnss: Add qcom,wcn3660a compatible
    WCN3660A is a variant of WCN3660, but with the same regulator
    requirements as WCN3620/WCN3680/WCN3660B. As far as qcom_wcnss_iris
    is concerned we can just use qcom,wcn3680 (wcn3680_data).
    
    However, a separate compatible is needed for WCN3660A because
    the wcn36xx driver uses it to enable chip-specific functionality.
    In particular, it enables 802.11ac for qcom,wcn3680 which is not
    supported by WCN3660A.
    
    Signed-off-by: Matti Lehtimäki <matti.lehtimaki@gmail.com>

--- drivers/remoteproc/qcom_wcnss_iris.c
+++ drivers/remoteproc/qcom_wcnss_iris.c
@@ -97,6 +97,7 @@ void qcom_iris_disable(struct qcom_iris *iris)
 static const struct of_device_id iris_of_match[] = {
 	{ .compatible = "qcom,wcn3620", .data = &wcn3620_data },
 	{ .compatible = "qcom,wcn3660", .data = &wcn3660_data },
+	{ .compatible = "qcom,wcn3660a", .data = &wcn3680_data },
 	{ .compatible = "qcom,wcn3660b", .data = &wcn3680_data },
 	{ .compatible = "qcom,wcn3680", .data = &wcn3680_data },
 	{}
