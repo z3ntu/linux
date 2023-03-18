c1fa6fa7e274fb0784a818e1433d69be43098513
Bryan O'Donoghue <bryan.odonoghue@linaro.org>
2023-03-18 12:18:17 +0000

    FROMLIST v4 dt-bindings: usb: Add qcom,pmic-pdphy dt-binding header
    Adds a series of defines which are used in the DTS and pdphy driver for
    identifying interrupts.
    
    Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>

---
+++ include/dt-bindings/usb/typec/qcom,pmic-pdphy.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2023, Linaro Ltd. All rights reserved.
+ */
+
+#ifndef _DT_BINDINGS_QCOM_PMIC_PDPHY_H
+#define _DT_BINDINGS_QCOM_PMIC_PDPHY_H
+
+#define PMIC_PDPHY_SIG_TX_IRQ		0x0
+#define PMIC_PDPHY_SIG_RX_IRQ		0x1
+#define PMIC_PDPHY_MSG_TX_IRQ		0x2
+#define PMIC_PDPHY_MSG_RX_IRQ		0x3
+#define PMIC_PDPHY_MSG_TX_FAIL_IRQ	0x4
+#define PMIC_PDPHY_MSG_TX_DISCARD_IRQ	0x5
+#define PMIC_PDPHY_MSG_RX_DISCARD_IRQ	0x6
+#define PMIC_PDPHY_FR_SWAP_IRQ		0x7
+
+#endif
