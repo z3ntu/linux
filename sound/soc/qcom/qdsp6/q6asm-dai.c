7f14c06259344c091a81d5ec6ce85aad13fd7d6c
Luca Weiss <luca.weiss@fairphone.com>
2024-10-04 12:18:39 +0200

    Revert "HACK q6asm-dai: decrease available formats (24-bit etc)"
    This reverts commit cdd378ab8071ec6784b188a4ec2e4185bb820a9a.
    
    It was semi-loud but distorted and volume control from pulse didn't
    work. Let's check if with this reverted it works 'better' again

--- sound/soc/qcom/qdsp6/q6asm-dai.c
+++ sound/soc/qcom/qdsp6/q6asm-dai.c
@@ -128,12 +128,19 @@ static const struct snd_pcm_hardware q6asm_dai_hardware_playback = {
 #define Q6ASM_FEDAI_DRIVER(num) { \
 		.playback = {						\
 			.stream_name = "MultiMedia"#num" Playback",	\
-			.rates = (SNDRV_PCM_RATE_48000),		\
-			.formats = (SNDRV_PCM_FMTBIT_S24_LE),	\
-			.channels_min = 2,				\
-			.channels_max = 2,				\
-			.rate_min =     48000,				\
-			.rate_max =	48000,				\
+			.rates = (SNDRV_PCM_RATE_8000_48000 |		\
+				  SNDRV_PCM_RATE_12000 |		\
+				  SNDRV_PCM_RATE_24000 |		\
+				  SNDRV_PCM_RATE_88200 |		\
+				  SNDRV_PCM_RATE_96000 |		\
+				  SNDRV_PCM_RATE_176400 |		\
+				  SNDRV_PCM_RATE_192000),		\
+			.formats = (SNDRV_PCM_FMTBIT_S16_LE |	/* S16_LE doesn't work?? */	\
+					SNDRV_PCM_FMTBIT_S24_LE),	\
+			.channels_min = 1,				\
+			.channels_max = 8,				\
+			.rate_min =     8000,				\
+			.rate_max =	192000,				\
 		},							\
 		.capture = {						\
 			.stream_name = "MultiMedia"#num" Capture",	\
