b6488a6e122d9c67433e797172584f87eb7f8adf
Luca Weiss <luca.weiss@fairphone.com>
2025-07-18 10:37:34 +0200

    fixup! fixup! clk: qcom: common: Add support to register rcg dfs in qcom_cc_really_probe

--- drivers/clk/qcom/common.c
+++ drivers/clk/qcom/common.c
@@ -390,7 +390,9 @@ int qcom_cc_really_probe(struct device *dev,
 			goto put_rpm;
 	}
 
-	if (desc->driver_data->dfs_rcgs && desc->driver_data->num_dfs_rcgs) {
+	if (desc->driver_data &&
+	    desc->driver_data->dfs_rcgs &&
+	    desc->driver_data->num_dfs_rcgs) {
 		ret = qcom_cc_register_rcg_dfs(regmap,
 					       desc->driver_data->dfs_rcgs,
 					       desc->driver_data->num_dfs_rcgs);
