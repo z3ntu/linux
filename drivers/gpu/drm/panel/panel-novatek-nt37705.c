e0b6b1a246dc614bf91c51b3a60722b54ab4421b
Luca Weiss <luca.weiss@fairphone.com>
2026-03-26 17:50:22 +0100

    clean up panel driver naming

--- drivers/gpu/drm/panel/panel-novatek-nt37705.c
+++ drivers/gpu/drm/panel/panel-novatek-nt37705.c
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: GPL-2.0-only
-// Copyright (c) 2026 FIXME
-// Generated with linux-mdss-dsi-panel-driver-generator from vendor device tree:
-//   Copyright (c) 2013, The Linux Foundation. All rights reserved. (FIXME)
+/*
+ * Generated with linux-mdss-dsi-panel-driver-generator from vendor device tree.
+ * Copyright (c) 2026 Luca Weiss <luca.weiss@fairphone.com>
+ */
 
 #include <linux/backlight.h>
 #include <linux/delay.h>
@@ -19,7 +20,7 @@
 #include <drm/drm_panel.h>
 #include <drm/drm_probe_helper.h>
 
-struct nt37705_boe_amoled {
+struct nt37705_panel {
 	struct drm_panel panel;
 	struct mipi_dsi_device *dsi;
 	struct drm_dsc_config dsc;
@@ -27,19 +28,18 @@ struct nt37705_boe_amoled {
 	struct gpio_desc *reset_gpio;
 };
 
-static const struct regulator_bulk_data nt37705_boe_amoled_supplies[] = {
+static const struct regulator_bulk_data nt37705_supplies[] = {
 	{ .supply = "vddio" },
 	{ .supply = "dvdd" },
 	{ .supply = "vci" },
 };
 
-static inline
-struct nt37705_boe_amoled *to_nt37705_boe_amoled(struct drm_panel *panel)
+static inline struct nt37705_panel *to_nt37705_panel(struct drm_panel *panel)
 {
-	return container_of_const(panel, struct nt37705_boe_amoled, panel);
+	return container_of_const(panel, struct nt37705_panel, panel);
 }
 
-static void nt37705_boe_amoled_reset(struct nt37705_boe_amoled *ctx)
+static void nt37705_reset(struct nt37705_panel *ctx)
 {
 	gpiod_set_value_cansleep(ctx->reset_gpio, 0);
 	usleep_range(10000, 11000);
@@ -49,7 +49,7 @@ static void nt37705_boe_amoled_reset(struct nt37705_boe_amoled *ctx)
 	usleep_range(10000, 11000);
 }
 
-static int nt37705_boe_amoled_on(struct nt37705_boe_amoled *ctx)
+static int nt37705_on(struct nt37705_panel *ctx)
 {
 	struct mipi_dsi_multi_context dsi_ctx = { .dsi = ctx->dsi };
 
@@ -175,7 +175,7 @@ static int nt37705_boe_amoled_on(struct nt37705_boe_amoled *ctx)
 	return dsi_ctx.accum_err;
 }
 
-static int nt37705_boe_amoled_off(struct nt37705_boe_amoled *ctx)
+static int nt37705_off(struct nt37705_panel *ctx)
 {
 	struct mipi_dsi_multi_context dsi_ctx = { .dsi = ctx->dsi };
 
@@ -187,26 +187,26 @@ static int nt37705_boe_amoled_off(struct nt37705_boe_amoled *ctx)
 	return dsi_ctx.accum_err;
 }
 
-static int nt37705_boe_amoled_prepare(struct drm_panel *panel)
+static int nt37705_prepare(struct drm_panel *panel)
 {
-	struct nt37705_boe_amoled *ctx = to_nt37705_boe_amoled(panel);
+	struct nt37705_panel *ctx = to_nt37705_panel(panel);
 	struct device *dev = &ctx->dsi->dev;
 	struct drm_dsc_picture_parameter_set pps;
 	int ret;
 
-	ret = regulator_bulk_enable(ARRAY_SIZE(nt37705_boe_amoled_supplies), ctx->supplies);
+	ret = regulator_bulk_enable(ARRAY_SIZE(nt37705_supplies), ctx->supplies);
 	if (ret < 0) {
 		dev_err(dev, "Failed to enable regulators: %d\n", ret);
 		return ret;
 	}
 
-	nt37705_boe_amoled_reset(ctx);
+	nt37705_reset(ctx);
 
-	ret = nt37705_boe_amoled_on(ctx);
+	ret = nt37705_on(ctx);
 	if (ret < 0) {
 		dev_err(dev, "Failed to initialize panel: %d\n", ret);
 		gpiod_set_value_cansleep(ctx->reset_gpio, 1);
-		regulator_bulk_disable(ARRAY_SIZE(nt37705_boe_amoled_supplies), ctx->supplies);
+		regulator_bulk_disable(ARRAY_SIZE(nt37705_supplies), ctx->supplies);
 		return ret;
 	}
 
@@ -229,23 +229,23 @@ static int nt37705_boe_amoled_prepare(struct drm_panel *panel)
 	return 0;
 }
 
-static int nt37705_boe_amoled_unprepare(struct drm_panel *panel)
+static int nt37705_unprepare(struct drm_panel *panel)
 {
-	struct nt37705_boe_amoled *ctx = to_nt37705_boe_amoled(panel);
+	struct nt37705_panel *ctx = to_nt37705_panel(panel);
 	struct device *dev = &ctx->dsi->dev;
 	int ret;
 
-	ret = nt37705_boe_amoled_off(ctx);
+	ret = nt37705_off(ctx);
 	if (ret < 0)
 		dev_err(dev, "Failed to un-initialize panel: %d\n", ret);
 
 	gpiod_set_value_cansleep(ctx->reset_gpio, 1);
-	regulator_bulk_disable(ARRAY_SIZE(nt37705_boe_amoled_supplies), ctx->supplies);
+	regulator_bulk_disable(ARRAY_SIZE(nt37705_supplies), ctx->supplies);
 
 	return 0;
 }
 
-static const struct drm_display_mode nt37705_boe_amoled_mode = {
+static const struct drm_display_mode nt37705_mode = {
 	.clock = (1116 + 100 + 30 + 100) * (2484 + 70 + 48 + 70) * 120 / 1000,
 	.hdisplay = 1116,
 	.hsync_start = 1116 + 100,
@@ -260,19 +260,19 @@ static const struct drm_display_mode nt37705_boe_amoled_mode = {
 	.type = DRM_MODE_TYPE_DRIVER,
 };
 
-static int nt37705_boe_amoled_get_modes(struct drm_panel *panel,
-					struct drm_connector *connector)
+static int nt37705_get_modes(struct drm_panel *panel,
+			     struct drm_connector *connector)
 {
-	return drm_connector_helper_get_modes_fixed(connector, &nt37705_boe_amoled_mode);
+	return drm_connector_helper_get_modes_fixed(connector, &nt37705_mode);
 }
 
-static const struct drm_panel_funcs nt37705_boe_amoled_panel_funcs = {
-	.prepare = nt37705_boe_amoled_prepare,
-	.unprepare = nt37705_boe_amoled_unprepare,
-	.get_modes = nt37705_boe_amoled_get_modes,
+static const struct drm_panel_funcs nt37705_panel_funcs = {
+	.prepare = nt37705_prepare,
+	.unprepare = nt37705_unprepare,
+	.get_modes = nt37705_get_modes,
 };
 
-static int nt37705_boe_amoled_bl_update_status(struct backlight_device *bl)
+static int nt37705_bl_update_status(struct backlight_device *bl)
 {
 	struct mipi_dsi_device *dsi = bl_get_data(bl);
 	u16 brightness = backlight_get_brightness(bl);
@@ -289,32 +289,12 @@ static int nt37705_boe_amoled_bl_update_status(struct backlight_device *bl)
 	return 0;
 }
 
-// TODO: Check if /sys/class/backlight/.../actual_brightness actually returns
-// correct values. If not, remove this function.
-static int nt37705_boe_amoled_bl_get_brightness(struct backlight_device *bl)
-{
-	struct mipi_dsi_device *dsi = bl_get_data(bl);
-	u16 brightness;
-	int ret;
-
-	dsi->mode_flags &= ~MIPI_DSI_MODE_LPM;
-
-	ret = mipi_dsi_dcs_get_display_brightness_large(dsi, &brightness);
-	if (ret < 0)
-		return ret;
-
-	dsi->mode_flags |= MIPI_DSI_MODE_LPM;
-
-	return brightness;
-}
-
-static const struct backlight_ops nt37705_boe_amoled_bl_ops = {
-	.update_status = nt37705_boe_amoled_bl_update_status,
-	.get_brightness = nt37705_boe_amoled_bl_get_brightness,
+static const struct backlight_ops nt37705_bl_ops = {
+	.update_status = nt37705_bl_update_status,
 };
 
 static struct backlight_device *
-nt37705_boe_amoled_create_backlight(struct mipi_dsi_device *dsi)
+nt37705_create_backlight(struct mipi_dsi_device *dsi)
 {
 	struct device *dev = &dsi->dev;
 	const struct backlight_properties props = {
@@ -324,24 +304,24 @@ nt37705_boe_amoled_create_backlight(struct mipi_dsi_device *dsi)
 	};
 
 	return devm_backlight_device_register(dev, dev_name(dev), dev, dsi,
-					      &nt37705_boe_amoled_bl_ops, &props);
+					      &nt37705_bl_ops, &props);
 }
 
-static int nt37705_boe_amoled_probe(struct mipi_dsi_device *dsi)
+static int nt37705_probe(struct mipi_dsi_device *dsi)
 {
 	struct device *dev = &dsi->dev;
-	struct nt37705_boe_amoled *ctx;
+	struct nt37705_panel *ctx;
 	int ret;
 
-	ctx = devm_drm_panel_alloc(dev, struct nt37705_boe_amoled, panel,
-				   &nt37705_boe_amoled_panel_funcs,
+	ctx = devm_drm_panel_alloc(dev, struct nt37705_panel, panel,
+				   &nt37705_panel_funcs,
 				   DRM_MODE_CONNECTOR_DSI);
 	if (IS_ERR(ctx))
 		return PTR_ERR(ctx);
 
 	ret = devm_regulator_bulk_get_const(dev,
-					    ARRAY_SIZE(nt37705_boe_amoled_supplies),
-					    nt37705_boe_amoled_supplies,
+					    ARRAY_SIZE(nt37705_supplies),
+					    nt37705_supplies,
 					    &ctx->supplies);
 	if (ret < 0)
 		return ret;
@@ -361,7 +341,7 @@ static int nt37705_boe_amoled_probe(struct mipi_dsi_device *dsi)
 
 	ctx->panel.prepare_prev_first = true;
 
-	ctx->panel.backlight = nt37705_boe_amoled_create_backlight(dsi);
+	ctx->panel.backlight = nt37705_create_backlight(dsi);
 	if (IS_ERR(ctx->panel.backlight))
 		return dev_err_probe(dev, PTR_ERR(ctx->panel.backlight),
 				     "Failed to create backlight\n");
@@ -396,9 +376,9 @@ static int nt37705_boe_amoled_probe(struct mipi_dsi_device *dsi)
 	return 0;
 }
 
-static void nt37705_boe_amoled_remove(struct mipi_dsi_device *dsi)
+static void nt37705_remove(struct mipi_dsi_device *dsi)
 {
-	struct nt37705_boe_amoled *ctx = mipi_dsi_get_drvdata(dsi);
+	struct nt37705_panel *ctx = mipi_dsi_get_drvdata(dsi);
 	int ret;
 
 	ret = mipi_dsi_detach(dsi);
@@ -408,22 +388,21 @@ static void nt37705_boe_amoled_remove(struct mipi_dsi_device *dsi)
 	drm_panel_remove(&ctx->panel);
 }
 
-static const struct of_device_id nt37705_boe_amoled_of_match[] = {
+static const struct of_device_id nt37705_of_match[] = {
 	{ .compatible = "boe,bj631jhm-t71-d900" },
 	{ /* sentinel */ }
 };
-MODULE_DEVICE_TABLE(of, nt37705_boe_amoled_of_match);
+MODULE_DEVICE_TABLE(of, nt37705_of_match);
 
-static struct mipi_dsi_driver nt37705_boe_amoled_driver = {
-	.probe = nt37705_boe_amoled_probe,
-	.remove = nt37705_boe_amoled_remove,
+static struct mipi_dsi_driver nt37705_driver = {
+	.probe = nt37705_probe,
+	.remove = nt37705_remove,
 	.driver = {
-		.name = "panel-nt37705-boe-amoled",
-		.of_match_table = nt37705_boe_amoled_of_match,
+		.name = "panel-novatek-nt37705",
+		.of_match_table = nt37705_of_match,
 	},
 };
-module_mipi_dsi_driver(nt37705_boe_amoled_driver);
+module_mipi_dsi_driver(nt37705_driver);
 
-MODULE_AUTHOR("linux-mdss-dsi-panel-driver-generator <fix@me>"); // FIXME
-MODULE_DESCRIPTION("DRM driver for nt37705 amoled command mode dsi panel");
+MODULE_DESCRIPTION("DRM driver for NT37705-equipped DSI panels");
 MODULE_LICENSE("GPL");
